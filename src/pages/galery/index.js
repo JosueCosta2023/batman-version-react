4298b257fef80ba6ccaf61925803f7530a223766
Josué Ocanha Costa <contato_josuecosta@hotmail.com>
2024-03-10 21:13:37 -0400

    up

--- src/pages/galery/index.js
+++ src/pages/galery/index.js
@@ -3,11 +3,13 @@ import Header from "../../components/header"
 import Footer from "../../components/footer"
 import imageJson from '../../assets/jsonArchives/cardImages.json'
 import './style.css'
+import imagemOffline from '../../assets/images/defaultImage.jpg'
+
 
 function Galery() {
-
+    
     const [data, setData] = useState([]);
-
+    
     useEffect(() => {
         setData(imageJson.imagens)
     }, [])
@@ -18,7 +20,12 @@ function Galery() {
                 <div className="content-imagens">
                     {data.map((personagem, index) => (
                         <div key={index} className="imagem-card">
-                             <img className="imagem" alt={personagem.name} title={personagem.descricao} src={personagem.url}/>          
+                            {
+                                personagem.url ? 
+                                <img className="imagem" alt={personagem.name} title={personagem.descricao} src={personagem.url}/>          
+                                :
+                                <img className="imagem" alt="Imagem Não carregada" title="Carregamento de desta imagem falhou" src={imagemOffline} />
+                            }
                         </div>
                     ))}
                 </div>
